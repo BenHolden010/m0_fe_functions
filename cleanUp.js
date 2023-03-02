6d5d31d1bce4be1d1ea98bac93bf986aa61ed642
Benjamin holden <benjamin.f.holden@outlook.com>
2023-03-02 14:07:12 -0700

    Add answers to cleanUp.js

--- cleanUp.js
+++ cleanUp.js
@@ -8,37 +8,44 @@
 
 
 // EX 1:
-function askForName() { console.log("Hello, what is your name?") }
+// function askForName() { 
+//   console.log("Hello, what is your name?") 
+// }
 
-askForName()
+// askForName()
 
+// Improper formating, console.log needed to be moved to the nest line.
 
 // EX 2:
-function addThreeNums(first, second, third) {
-var sum = first + second + third
-return sum;
-      }
+// function addThreeNums(first, second, third) {
+// var sum = first + second + third;
+// return sum;
+//       }
 
-addThreeNums(1, 2, 3);
-addThreeNums(4, 2, 7);
+// console.log(addThreeNums(1, 2, 3));
+// console.log(addThreeNums(4, 2, 7));
 
+// without a console.log statement, nothing will print, console.log was addad to two of the imput statements at the bottom.
 
 // EX 3:
-func makeFreshPesto(){
-  console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
-  console.log("Pulse basil and pine nuts");
-  console.log("Add garlic and cheeses");
-  console.log("Slowly pour in oil");
-  console.log("Season");    }
+// function makeFreshPesto(){
+//   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
+//   console.log("Pulse basil and pine nuts");
+//   console.log("Add garlic and cheeses");
+//   console.log("Slowly pour in oil");
+//   console.log("Season");    }
 
-makeFreshPesto();
-
+// makeFreshPesto();
+// func was not spelled out fully solution = function;
 
 //  EX 4:
-function average(num1, num2) 
+function avg(num1, num2) 
   {
 var sum = num1 + num2;
     var avg = sum / 2;
 
 return avg
   }
+  console.log(avg(3,7))
+
+// avg was not defined, i changed average to avg so the functions would align, then created a console.log input statement to ensure the data structure worked.
